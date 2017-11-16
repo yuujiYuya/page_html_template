6b61d38bd2a5b187594545667b4c8c3821c84ab2
yuujiYuya <s1610117@jaist.ac.jp>
2017-11-17 06:48:03 +0900

    初めてのHTMLテンプレート

---
+++ assert/js/offcanvas.js
@@ -0,0 +1,12 @@
+/*!
+ * Bootstrap v4.0.0-alpha.6 (https://getbootstrap.com)
+ * Copyright 2011-2017 The Bootstrap Authors
+ * Copyright 2011-2017 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+/*! normalize.css v5.0.0 | MIT License | github.com/necolas/normalize.css */
+$(document).ready(function () {
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('.row-offcanvas').toggleClass('active')
+  });
+});
